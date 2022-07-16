671fcd271958cbd9994d728bb380a87945ab21f4
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-07-16 12:07:12 -0500

    Creating package.json file in root directory, adding concurrently as a dependency and adding start, develop, install and build scripts. Modifying start script in server/package.json and adding watch script. Adding proxy field to client/package.json, and adding bootstrap and react-bootstrap as dependencies. Adding code in server.js to serve client/build as static assets if we are in production environment. Adding code in routes/index.js to serve up react front-end in production.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-
+const path = require('path');
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(routes);
 
