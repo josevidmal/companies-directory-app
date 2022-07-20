418f1d041694891bb02b0aec2a82d85211c21706
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-07-20 00:18:59 -0500

    Adding className footer-list-item to ListGroup.Item elements in components/Footer/index.js. Adding background-color property to body element selector in index.css file. Adding code to App.css file, with some css properties for the layout and size of elemtns in the Header and Footer components, as well as css properties for backgroung colors and text colors.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,9 +7,9 @@ const Footer = () => {
         <footer id="footer-container" className="containers">
             <h4 id="footer-title" className="headings">Reto Técnico realizado por:<br></br>José Vidal Maza Alonso<br></br>Full Stack Web Developer</h4>
             <ListGroup horizontal id="footer-list" className="lists">
-                <ListGroup.Item><a id="github-link" className="footer-links" href="https://github.com/josevidmal"><FaGithub /></a></ListGroup.Item>
-                <ListGroup.Item><a id="linkedin-link" className="footer-links" href="https://www.linkedin.com/in/jose-vidal-maza-alonso-73477646/"><FaLinkedin /></a></ListGroup.Item>
-                <ListGroup.Item><a id="stackoverflow-link" className="footer-links" href="https://stackoverflow.com/users/17156054/jos%c3%a9-vidal"><FaStackOverflow /></a></ListGroup.Item>
+                <ListGroup.Item className="footer-list-item"><a id="github-link" className="footer-links" href="https://github.com/josevidmal"><FaGithub /></a></ListGroup.Item>
+                <ListGroup.Item className="footer-list-item"><a id="linkedin-link" className="footer-links" href="https://www.linkedin.com/in/jose-vidal-maza-alonso-73477646/"><FaLinkedin /></a></ListGroup.Item>
+                <ListGroup.Item className="footer-list-item"><a id="stackoverflow-link" className="footer-links" href="https://stackoverflow.com/users/17156054/jos%c3%a9-vidal"><FaStackOverflow /></a></ListGroup.Item>
             </ListGroup>
         </footer>
     );
